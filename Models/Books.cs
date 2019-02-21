71c892af612d3f54e237aff1118a8a7b841f0716
Jeff Hachtel <hachtel.5@osu.edu>
2019-02-20 20:32:23 -0500

    Added Book Model

---
+++ Models/Books.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+
+        public string Title { get; set; }
+
+        public string SubTitle { get; set; }
+
+    }
+}
