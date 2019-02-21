95b9fcdf0cb338a4b6bf1c95a4082f3c29f0b6aa
Jeff Hachtel <hachtel.5@osu.edu>
2019-02-20 20:58:11 -0500

    Added BooksController, deleted ValuesController

---
+++ api/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fisher.Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Api.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
