ca092331e26775dc033273a478324638af26b4f7
Jeff Hachtel <hachtel.5@osu.edu>
2019-02-28 14:01:19 -0500

    added book row

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BooksComponent } from './books/books/books.component';
+import { BookRowComponent } from './books/book-row/book-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BooksComponent
+    BooksComponent,
+    BookRowComponent
   ],
   imports: [
     BrowserModule
