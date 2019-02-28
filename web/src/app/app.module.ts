7279d0c31e2a099a492c896795aa6293375e98c2
Jeff Hachtel <hachtel.5@osu.edu>
2019-02-28 13:42:59 -0500

    added books component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books/books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule
